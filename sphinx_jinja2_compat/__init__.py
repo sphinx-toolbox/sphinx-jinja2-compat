34ff1def5d0b021dc18fe672e5b38281a6ae3994
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-03-29 22:16:25 +0100

    Lint

--- sphinx_jinja2_compat/__init__.py
+++ sphinx_jinja2_compat/__init__.py
@@ -27,9 +27,9 @@ Patches Jinja2 v3 to restore compatibility with earlier Sphinx versions.
 #
 
 # stdlib
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, List, TypeVar
 
-__all__ = []
+__all__: List[str] = []
 
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2022 Dominic Davis-Foster"
@@ -40,32 +40,32 @@ __email__: str = "dominic@davis-foster.co.uk"
 F = TypeVar('F', bound=Callable[..., Any])
 
 # 3rd party
-import markupsafe
+import markupsafe  # noqa: E402
 
 if not hasattr(markupsafe, "soft_unicode"):
 
 	def soft_unicode(s: Any) -> str:
-		return markupsafe.utils.soft_str(s)
+		return markupsafe.soft_str(s)
 
-	markupsafe.soft_unicode = soft_unicode
+	markupsafe.soft_unicode = soft_unicode  # type: ignore[attr-defined]
 
 # 3rd party
-import jinja2
-import jinja2.filters
-import jinja2.utils
+import jinja2  # noqa: E402
+import jinja2.filters  # noqa: E402
+import jinja2.utils  # noqa: E402
 
 if not hasattr(jinja2.filters, "environmentfilter"):
 
 	def environmentfilter(f: F) -> F:
 		return jinja2.utils.pass_environment(f)
 
-	jinja2.filters.environmentfilter = environmentfilter
-	jinja2.environmentfilter = environmentfilter
+	jinja2.filters.environmentfilter = environmentfilter  # type: ignore[attr-defined]
+	jinja2.environmentfilter = environmentfilter  # type: ignore[attr-defined]
 
 if not hasattr(jinja2.utils, "contextfunction"):
 
 	def contextfunction(f: F) -> F:
 		return jinja2.utils.pass_context(f)
 
-	jinja2.utils.contextfunction = contextfunction
-	jinja2.contextfunction = contextfunction
+	jinja2.utils.contextfunction = contextfunction  # type: ignore[attr-defined]
+	jinja2.contextfunction = contextfunction  # type: ignore[attr-defined]
