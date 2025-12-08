e84c1286a31777ebb14e53b2490308a262cfa921
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-08 18:04:13 +0000

    Change type: ignore for sphinx_prompt

--- sphinx_jinja2_compat/__init__.py
+++ sphinx_jinja2_compat/__init__.py
@@ -85,7 +85,7 @@ if "NO_SPHINX_JINJA2_COMPAT" not in os.environ:
 
 	try:
 		# 3rd party
-		import sphinx_prompt  # type: ignore[import-untyped]
+		import sphinx_prompt  # type: ignore[import-not-found]
 	except ImportError:
 		pass
 	else:
